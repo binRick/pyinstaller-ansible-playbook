0f8113dab408be2adfd066fd337d42d3638ffb0a
root <root@f35.vpnservice.company>
2021-11-22 19:53:31 -0500

    add build script and test cases

---
+++ Build.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+set -e
+eval "$(cat utils.sh)"
+
+TEST_PB="$(
+	cat <<EOF
+---
+name: test playbook
+hosts: localhost
+connection: local
+gather_facts: no
+tasks:
+  - name: id
+    command: id
+  - debug: var=id.stdout
+EOF
+)"
+
+setup() {
+	export ANSIBLE_PYTHON_INTERPRETER=$(command -v python3)
+	if [[ ! -d .v ]]; then
+		python3 -m venv .v
+		source .v/bin/activate
+		pip install pip -U -q
+		pip install pyinstaller six ansible -q
+	fi
+
+	source .v/bin/activate
+}
+
+clean() {
+	[[ -d dist ]] && rm -rf dist
+	true
+}
+
+compile() {
+	do_exec "pyinstaller ansible.spec"
+	#	while read -r l; do
+	#		msg="$(ansi --yellow --italic "$l")"
+	#		echo -e "$msg"
+	#	done < <(pyinstaller ansible.spec)
+	ansi >&2 --magenta --bold "$(find dist -type f)"
+	(cd dist && ln -sf ansible-playbook ansible)
+}
+
+do_exec() {
+	(
+		set +e
+		cmd="$@"
+		fd_setup
+		stdout_file=$(mktemp)
+		stderr_file=$(mktemp)
+		ec_file=$(mktemp)
+		wrap_Exec "$cmd" "$stdout_file" "$stderr_file" "$ec_file"
+		ec=$?
+		err="$(cat $stderr_file)"
+		out="$(cat $stdout_file)"
+		return $ec
+	)
+}
+
+test() {
+	while read -r l; do
+		msg="$(ansi --green --bold "ANSIBLE> $l")"
+		echo -e "$msg"
+	done < <(
+		./dist/ansible --version
+	)
+	while read -r l; do
+		msg="$(ansi --magenta --bold "ANSIBLE PLAYBOOK> $l")"
+		echo -e "$msg"
+done < <(
+	./dist/ansible-playbook --version
+)
+	./dist/ansible all -i localhost, -c local -m ping
+}
+
+debug() {
+	echo -e "$(ansi --cyan --bold --bg-black "Test Playbook:  ")\n$(ansi --yellow --italic "$TEST_PB")"
+}
+
+common_pre_main() {
+	setup
+}
+
+prod_main() {
+	compile
+	debug
+	test
+}
+
+dev_main() {
+	clean
+	prod_main
+}
+
+main() {
+	common_pre_main
+	if [[ "$DEV_MODE" == 1 ]]; then
+		dev_main
+	else
+		prod_main
+	fi
+}
+
+main
---
+++ utils.sh
@@ -0,0 +1,61 @@
+fd_setup() {
+	exec 3>&1 4>&2 5>&2
+}
+loggerForStdout() {
+	local x
+	while IFS= read -r x; do
+		[[ "$x" == "" ]] && continue
+		x="$(ansi --green --bold "$x")"
+		printf "STDOUT\t%s\n" "$x"
+	done >&4
+}
+loggerForExitCode() {
+	local x
+	local cmd="$1"
+	while IFS= read -r x; do printf "EXIT_CODE\t'$cmd' => \t%d\n" "$x"; done >&5
+}
+loggerForStderr() {
+	local x
+	while IFS= read -r x; do
+		[[ "$x" == "" ]] && continue
+		x="$(ansi --magenta --bold "$x")"
+		printf "STDERR\t%s\n" "$x"
+	done >&2
+}
+
+wrap_Exec() {
+	local cmd="$1"
+	local stdout_file="$2"
+	local stderr_file="$3"
+	local ec_file="$4"
+	local cp=$$
+	( (
+		stdbuf -oL -eL env $(command -v bash) --norc --noprofile +x +e -c "$cmd"
+		{ echo -e "$?" | tee $ec_file >/dev/null; }
+	) | tee $stdout_file | loggerForStdout) 2>&1 | tee $stderr_file | loggerForStderr
+	cat $ec_file | loggerForExitCode "$cmd" >/dev/null
+	return $(cat $ec_file)
+}
+
+demo() {
+	fd_setup
+
+	stdout_file=$(mktemp)
+	stderr_file=$(mktemp)
+	ec_file=$(mktemp)
+	cmd="ls /a && ls /b; ls /c; ls /22;find /|head -n10;ls /11;"
+	cmd="ls /a"
+	cmd="ls /"
+	cmd="ls /;ls /a && ls /b; ls /c; ls /22;find /|head -n10;ls /11;"
+	wrap_Exec \
+		"$cmd" "$stdout_file" "$stderr_file" "$ec_file"
+	ec=$?
+	err="$(cat $stderr_file)"
+	out="$(cat $stdout_file)"
+
+	ansi --yellow "$cmd"
+	ansi --red "$err"
+	ansi --green "$out"
+	ansi --cyan "$ec"
+	exit $ec
+}
