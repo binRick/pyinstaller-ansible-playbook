e671d3a9aa210f313c9a619e1939ac630480891b
root <root@f212.vpnservice.company>
2021-12-03 18:25:41 -0500

    m

--- cp_binary_aliases.sh
+++ cp_binary_aliases.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -eou pipefail
 cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-BINARY=${1:-ansible-playbook}
 
+while read -r B; do
 while read -r f; do
 	while read -r a; do
 		df="xxxxxxx"
 		cmd="rsync $f $(dirname $f)/$a"
 		ansi >&2 --yellow --italic "$cmd"
 		eval "$cmd"
-	done < <(./get_binary_aliases.sh "$BINARY")
-
-done < <(find binaries -type f -name "$BINARY")
+	done < <(./get_binary_aliases.sh "$B")
+done < <(find binaries -type f -name "$B")
+done < <(./get_aliases.sh)
