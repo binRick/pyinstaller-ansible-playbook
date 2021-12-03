d499f0c2853c81b99c31ce3fa353322d4f334127
root <root@f35.vpnservice.company>
2021-12-03 15:44:54 -0500

    m

--- df.sh
+++ df.sh
@@ -2,10 +2,8 @@
 set -eou pipefail
 cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 DISTROS="fedora35 alpine315"
-DISTROS="fedora35"
 DOCKERFILES="builder ansible linodecli"
-DOCKERFILES="ansible"
-MODE=${1:-build}
+MODE=${1:-main}
 BUILD_ENV=
 
 if [[ -f .envrc ]]; then
@@ -17,13 +15,9 @@ fi
 
 build() {
 	for DISTRO in $DISTROS; do for DOCKERFILE in $DOCKERFILES; do
-		bd="$(pwd)/binaries/$DISTRO"
-		sd="$(pwd)/static-binaries/$DISTRO"
 		ff=$(mktemp)
 		find_cmd="docker run --rm $DISTRO-$DOCKERFILE:latest find /compile/dist /compile/dist-static -type f 2>/dev/null| tee $ff"
-		local_dir="[[ -d '$bd' ]] || mkdir -p '$bd'"
-		local_dir1="[[ -d '$sd' ]] || mkdir -p '$sd'"
-		cmd="docker build -f $DISTRO-$DOCKERFILE.Dockerfile -t $DISTRO-$DOCKERFILE --target $DISTRO-$DOCKERFILE $(echo -e "$BUILD_ENV"|tr '\n' ' ') . && eval $find_cmd && $local_dir &&  $local_dir1"
+		cmd="docker build -f $DISTRO-$DOCKERFILE.Dockerfile -t $DISTRO-$DOCKERFILE --target $DISTRO-$DOCKERFILE $(echo -e "$BUILD_ENV"|tr '\n' ' ') . && eval $find_cmd"
 		ansi >&2 --yellow --italic "$cmd"
 		eval "$cmd"
 	done; done
@@ -36,6 +30,8 @@ img_files() {
     eval "$find_cmd"| while read -r f; do
       local_path="./binaries/$DISTRO/$(basename $(dirname $f))/$(basename $f)"
       bn="$(basename $f)"
+      dn="$(dirname $local_path)"
+      [[ -d "$dn" ]] || mkdir -p "$dn"
       chmod_cmd="chmod 0700 $local_path && chown root:root $local_path"
       cat_cmd="docker run --rm $DISTRO-$DOCKERFILE:latest cat $f | pv > $local_path && $chmod_cmd && md5sum $local_path"
       cp_cmd="docker cp \$CID:$f $local_path"
@@ -57,20 +53,10 @@ EOF
 	done; done
 }
 
-files(){
-  while read -r img f; do
-    dd="./binaries/$(echo $img|cut -d'-' -f1)/$(basename $(dirname $f))"
-    [[ -d "$dd" ]] || mkdir -p "$dd"
-    cid=$(command docker create $img)
-		cmd="docker cp $cid:$f $dd/$(basename $f)||true; docker kill $cid 2>/dev/null||true"
-		ansi >&2 --yellow --italic "$cmd"
-    eval "$cmd"
-  done < <(img_files)
+
+main(){
+  set +e
+  build
+  img_files
 }
-
-#build
-#cp_img_files
-#do_find
-#build
-
 eval "$MODE"
